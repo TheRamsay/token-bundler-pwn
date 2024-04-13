f9442350c8f4b2bacd36a8920aaab9f1739bd5c3
TheRamsay <ramsayofbolton@gmail.com>
2024-04-13 16:03:05 +0200

    pip version fix

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,3 @@
-pip3 install -r requirements.txt
+pip3.9 install -r requirements.txt
 python3.9 ./token_bundler/manage.py makemigrations --noinput
 python3.9 ./token_bundler/manage.py migrate --noinput
