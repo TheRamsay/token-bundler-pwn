85850c930b7c9fb6ba401de9b495408fc075150b
TheRamsay <ramsayofbolton@gmail.com>
2024-04-13 16:07:02 +0200

    different pip call

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,3 @@
-pip3.9 install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 ./token_bundler/manage.py makemigrations --noinput
 python3.9 ./token_bundler/manage.py migrate --noinput
