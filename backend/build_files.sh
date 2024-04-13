6562473936ad83d48b4f0d3c633d07f19c164a99
TheRamsay <ramsayofbolton@gmail.com>
2024-04-13 16:09:07 +0200

    ensure pip

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,4 @@
+python3.9 -m ensurepip
 python3.9 -m pip install -r requirements.txt
 python3.9 ./token_bundler/manage.py makemigrations --noinput
 python3.9 ./token_bundler/manage.py migrate --noinput
